d8790b62fc9821211067c0da2c0a96b0cd713725
sssinsi <tet643773@gmail.com>
2016-03-15 18:56:35 +0900

    add selenium test.

---
+++ ci_tests/scripts/SeleneseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class SeleneseTests extends PHPUnit_Extensions_SeleniumTestCase{
+    // set html test directory
+    public static $seleneseDirectory = './html';
+
+    protected function setUp(){
+        $this->setHost('127.0.0.1');
+        $this->setPost('8080');
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl('http://localhost:8080');
+        $this->setTimeout('100');
+    }
+
+
+}
