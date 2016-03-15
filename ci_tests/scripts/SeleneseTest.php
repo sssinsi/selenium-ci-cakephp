8b720d14b1869fefdf17fbed31fca919884260f4
sssinsi <tet643773@gmail.com>
2016-03-15 20:27:09 +0900

    change port

--- ci_tests/scripts/SeleneseTest.php
+++ ci_tests/scripts/SeleneseTest.php
@@ -6,7 +6,7 @@ class SeleneseTests extends PHPUnit_Extensions_SeleniumTestCase{
 
     protected function setUp(){
         $this->setHost('127.0.0.1');
-        $this->setPost(8080);
+        $this->setPost(4444);
         $this->setBrowser('*firefox');
         $this->setBrowserUrl('http://localhost:8080');
         $this->setTimeout(100);
