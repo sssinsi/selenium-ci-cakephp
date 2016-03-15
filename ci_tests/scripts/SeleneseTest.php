0ae92be2fa1eae8d008143429b9e4ce984e7d500
sssinsi <tet643773@gmail.com>
2016-03-15 19:48:40 +0900

    add html test.

--- ci_tests/scripts/SeleneseTest.php
+++ ci_tests/scripts/SeleneseTest.php
@@ -2,15 +2,18 @@
 
 class SeleneseTests extends PHPUnit_Extensions_SeleniumTestCase{
     // set html test directory
-    public static $seleneseDirectory = './html';
+    public static $seleneseDirectory = './html/';
 
     protected function setUp(){
         $this->setHost('127.0.0.1');
-        $this->setPost('8080');
+        $this->setPost(8080);
         $this->setBrowser('*firefox');
         $this->setBrowserUrl('http://localhost:8080');
-        $this->setTimeout('100');
+        $this->setTimeout(100);
     }
 
-
+    public function testSeleniumFile(){
+        $this->open('http://localhost:8080');
+        $this->runSelenese('sample_test_case.html');
+    }
 }
